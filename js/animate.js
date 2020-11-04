23cec4e8ef4b1cac1c3751b971106a0a34c9bcc5
Lucy Yip <Lucy.Yip@ibm.com>
2020-11-04 00:09:20 -0500

    .

--- js/animate.js
+++ js/animate.js
@@ -4,3 +4,10 @@ function nav(number) {
         block: 'center'
     })
 }
+
+//https://github.com/michalsnik/aos
+AOS.init({
+    offset: 100,
+    easing: 'ease-in-quart',
+    duration: 800,
+});
--- js/sketch.js
+++ js/sketch.js
@@ -1,28 +1,12 @@
-var displayword = "BLACK LIFE MATTERS";
-const showText = false; 
-const textAlpha = 0.1; //the alpha of the text if displayed (low value will make it slowly fade in)
-const backgroundColor = 0; //kinda self-explanatory
-const strokeAlpha = 70; //the alpha of the lines (lower numbers are more transparent)
-const strokeColor = 254; //the line color
-
-
-const fontSampleFactor = 0.4; //How many points there are: the higher the number, the closer together they are (more detail)
-
-const noiseZoom = 0.006; //how zoomed in the perlin noise is
-const noiseOctaves = 4; //The number of octaves for the noise
-const noiseFalloff = 0.5; //The falloff for the noise layers
-
-const zOffsetChange = 0; //How much the noise field changes in the z direction each frame
-const individualZOffset = 0; //how far away the points/lines are from each other in the z noise axies (the bigger the number, the more chaotic)
-
-const lineSpeed = 0.6; //the maximum amount each point can move each frame
-
-const newPointsCount = 9; //the number of new points added when the mouse is dragged
-
-var seed;
-var font;
-var points = [];
-var startingPoints;
+var displayword = "BLACK LIVES MATTER",
+    density = 0.2,
+    fractalization = 0.005,
+    falloff = 0.5,
+    speed = 0.5,
+    seed,
+    font,
+    points = [],
+    startingPoints;
 
 function preload() {
     font = loadFont("../assets/LEMONMILK-Bold.OTF");
@@ -31,27 +15,22 @@ function preload() {
 function setup() {
     var myCanvas = createCanvas(windowWidth, windowHeight);
     myCanvas.parent("p5");
-
-    background(backgroundColor);
+    
     textFont(font);
     textSize(100);
-    fill(strokeColor, textAlpha);
-    stroke(strokeColor, strokeAlpha);
-    noiseDetail(noiseOctaves, noiseFalloff);
 
-    start();
-};
+    fill(255);
 
-function start() {
-    background(backgroundColor);
+    noiseDetail(10, falloff);
     randomSeed(seed);
     noiseSeed(seed);
-    frameCount = 0;
+
     startingPoints = font.textToPoints(displayword, width / 2 - textWidth(displayword) / 2, height / 2 + 40, 100, {
-        "sampleFactor": fontSampleFactor
+        "sampleFactor": density
     });
 
     points = [];
+
     for (let p = 0; p < startingPoints.length; p++) {
         points[p] = startingPoints[p];
         points[p].zOffset = random();
@@ -59,51 +38,41 @@ function start() {
 }
 
 function draw() {
-    background(0, 0, 0, 10);
-    
-    if (showText) {
-        noStroke();
-        text(displayword, width / 2 - textWidth(displayword) / 2, height / 2);
-        stroke(strokeColor, strokeAlpha);
+
+    background(0, 0, 0, 10 + frameCount / 100);
+    stroke(100 * cos(frameCount / 120) + 155);
+    strokeWeight(1);
+
+    if (pmouseX !== mouseX || mouseY !== mouseY) {
+        stroke(255);
+        strokeWeight(2);
     }
+
     for (let pt = 0; pt < points.length; pt++) {
         let p = points[pt];
-        let noiseX = p.x * noiseZoom;
-        let noiseY = p.y * noiseZoom;
-        let noiseZ = frameCount * zOffsetChange + p.zOffset * individualZOffset;
-        let newPX = p.x + map(noise(noiseX, noiseY, noiseZ), 0, 1, -lineSpeed, lineSpeed);
-        let newPY = p.y + map(noise(noiseX, noiseY, noiseZ + 214), 0, 1, -lineSpeed, lineSpeed);
+        let noiseX = p.x * fractalization;
+        let noiseY = p.y * fractalization;
+        let newPX = p.x + map(noise(noiseX, noiseY, 0), 0, 1, -speed, speed);
+        let newPY = p.y + map(noise(noiseX, noiseY, 255), 0, 1, -speed, speed);
         line(p.x, p.y, newPX, newPY);
         p.x = newPX;
         p.y = newPY;
     }
-}
 
-function keyPressed() {
-    if (keyIsDown(CONTROL) && key.toLowerCase() === 's') {
-        save();
-    } else if (keyCode === BACKSPACE || keyCode === DELETE) {
-        displayword = displayword.subdisplayword(0, displayword.length - 1);
-        start();
-    } else if (keyCode === RETURN) {
-        seed = floor(random(1000000));
-        start();
-    } else if (keyCode === 32 || keyCode >= 186 || (keyCode >= 48 && keyCode <= 90)) {
-        displayword += key;
-        start();
-    }
-}
-
-function mouseDragged() {
-    for (let i = 0; i < newPointsCount; i++) {
-        let angle = random(TAU);
-        let magnitude = randomGaussian() * ((newPointsCount - 1) ** 0.5 * 3);
-        let newPoint = {
-            "x": mouseX + magnitude * cos(angle),
-            "y": mouseY + magnitude * sin(angle),
-            "zOffset": random()
+    /*
+    if (frameCount % 60 == 0) {
+        for (let i = 0; i < 5; i++) {
+            let angle = random(TAU);
+            let magnitude = randomGaussian() * ((5 - 1) ** 0.5 * 3);
+            let newPoint = {
+                "x": mouseX + magnitude * cos(angle),
+                "y": mouseY + magnitude * sin(angle),
+                "zOffset": random()
+            };
+            points[points.length] = newPoint;
+            startingPoints[startingPoints.length] = newPoint;
         };
-        points[points.length] = newPoint;
-        startingPoints[startingPoints.length] = newPoint;
-    }
+    };
+    */
+
 }

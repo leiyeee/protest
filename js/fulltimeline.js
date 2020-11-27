8e493080bf058b3e73b11c40310e26f3d85ee48b
leiyeee <leiye@mit.edu>
2020-11-27 01:28:33 -0500

    sss

--- js/fulltimeline.js
+++ js/fulltimeline.js
@@ -20,147 +20,151 @@ function fulltimeline() { // hi this is lucy
     svg.style("overflow", "hidden")
         .style("z-index", 0)
 
+    $('#yipcountries').click(function () {
+       var selected = $("input[type='radio'][name='countries']:checked").val();
 
-    d3.csv("data/ssp_simplified.csv").then(function (data) {
 
-        let actions = [],
-            countries = [];
+        d3.csv("data/ssp_simplified.csv").then(function (data) {
 
-        data.forEach(function (d) {
-            if (actions.includes(d.action) == false) {
-                actions.push(d.action);
-            }
-            if (countries.includes(d.country_name) == false) {
-                countries.push(d.country_name);
-            }
-        });
-        
-        console.log(countries);
+            let actions = [],
+                countries = [];
 
-        data = data.filter(function (d) {
-            return d.date_text.includes("unknown") == false &&
-                d.country_name == "China" &&
-                d.initiator.includes("unknown") == false;
-        })
-
-        var actionScale = d3.scaleOrdinal()
-            .domain(actions)
-            .range(d3.schemeSet3);
-
-        var timeScale = d3.scaleTime()
-            .domain(d3.extent(data, function (d) {
-                return parseTime(+d.date);
-            }))
-            .range([margin, width - margin]);
-
-        var violenceScale = d3.scaleLinear()
-            .domain(d3.extent(data, function (d) {
-                return d.violence_policeorstate;
-            }))
-            .range([3, 10]);
-
-        var xaxistext = svg.append("text")
-            .attr("class", "xaxistext")
-            .attr("x", width - margin / 2)
-            .attr("y", height - margin / 2)
-            .text("Timeline")
-            .style("text-anchor", "end")
-            .style("fill", "#bcbcbc");
-
-        var xaxis = svg.append("g")
-            .attr("class", "yipxaxis")
-            .attr("transform", "translate(0," + height * 0.8 + ")")
-            .call(d3.axisBottom(timeScale))
-            .style("opacity", 0)
-            .transition()
-            .delay(3000)
-            .style("opacity", 1);
-
-        xaxis.transition().selectAll(".yipxaxis line")
-            .style("stroke-width", 0.1)
-            .style("stroke", "#bcbcbc")
-            .style("stroke-dasharray", "1,5")
-            .attr("y1", -5)
-            .attr("y2", -height * 0.3);
-
-        var node = svg.append("g")
-            .selectAll("circle")
-            .data(data)
-            .enter()
-            .append("circle")
-            .attr("class", function (d) {
-                return "nodes " + d.eventid;
-            })
-            .attr("r", function (d) {
-                return violenceScale(d.violence_policeorstate);
-            })
-            .attr("cx", width / 2)
-            .attr("cy", height / 2)
-            .style("fill", function (d) {
-                return actionScale(d.action);
-            })
-
-        var simulation = d3.forceSimulation()
-            .force("x",
-                d3.forceX()
-                .strength(0.8)
-                .x(function (d) {
-                    return timeScale(parseTime(+d.date));
-                }))
-            .force("y",
-                d3.forceY()
-                .strength(0.1)
-                .y(height / 2))
-            .force("charge", d3.forceManyBody().strength(0.8))
-            .force("collide", d3.forceCollide().strength(1)
-                .radius(function (d) {
-                    return violenceScale(d.violence_policeorstate) + 1;
-                })
-                .iterations(2))
-
-        simulation
-            .nodes(data)
-            .on("tick", function (d) {
-                node
-                    .attr("cx", function (d) {
-                        return d.x;
-                    })
-                    .attr("cy", function (d) {
-                        return d.y;
-                    })
+            data.forEach(function (d) {
+                if (actions.includes(d.action) == false) {
+                    actions.push(d.action);
+                }
+                if (countries.includes(d.country_name) == false) {
+                    countries.push(d.country_name);
+                }
             });
 
-        node
-            .on("mouseover", function (event, d) {
+            console.log(countries);
 
-                console.log(d);
-
-                let tip = "<svg height='10' width='20'><polygon class='tip' points='10,0,20,10,0,10'/></svg>";
-
-                tooltip.html("<div class='tipp'><div class='tip'>" + tip + "<p class='tooltiptext'>" + d.date_text + " (" + d.day_span + " days) <br><b>" + d.action + "</b> by " + d.initiator + " (" + d.initiator_type + ")<br>" + d.location + "</div></div>");
-
-                tooltip
-                    .style("top", (+this.attributes.cy.value + 30) + "px")
-                    .style("left", (+this.attributes.cx.value - 100) + "px")
-                    .transition()
-                    .style("opacity", 1);
-
-            })
-            .on("mouseout", function (event, d) {
-                tooltip.transition().style("opacity", 0).transition().style("top", 0 + "px")
-            })
-            .on("click", function (d) {
-                let button = document.createElement("button");
-                button.className = "policy bx--btn--primary btnn";
-                button.innerHTML = d.intervention;
-
-                document.getElementById("selection").appendChild(button);
-                document.getElementById("newPolicy").style.display = "block";
-                document.getElementById("pBuild").innerHTML = "Build";
-
-                d3.select(this).transition().style("fill", "white");
+            data = data.filter(function (d) {
+                return d.date_text.includes("unknown") == false &&
+                    d.country_name == selected &&
+                    d.initiator.includes("unknown") == false;
             })
 
+            var actionScale = d3.scaleOrdinal()
+                .domain(actions)
+                .range(d3.schemeSet3);
+
+            var timeScale = d3.scaleTime()
+                .domain(d3.extent(data, function (d) {
+                    return parseTime(+d.date);
+                }))
+                .range([margin, width - margin]);
+
+            var violenceScale = d3.scaleLinear()
+                .domain(d3.extent(data, function (d) {
+                    return d.violence_policeorstate;
+                }))
+                .range([3, 10]);
+
+            var xaxistext = svg.append("text")
+                .attr("class", "xaxistext")
+                .attr("x", width - margin / 2)
+                .attr("y", height - margin / 2)
+                .text("Timeline")
+                .style("text-anchor", "end")
+                .style("fill", "#bcbcbc");
+
+            var xaxis = svg.append("g")
+                .attr("class", "yipxaxis")
+                .attr("transform", "translate(0," + height * 0.8 + ")")
+                .call(d3.axisBottom(timeScale))
+                .style("opacity", 0)
+                .transition()
+                .delay(3000)
+                .style("opacity", 1);
+
+            xaxis.transition().selectAll(".yipxaxis line")
+                .style("stroke-width", 0.1)
+                .style("stroke", "#bcbcbc")
+                .style("stroke-dasharray", "1,5")
+                .attr("y1", -5)
+                .attr("y2", -height * 0.3);
+
+            var node = svg.append("g")
+                .selectAll("circle")
+                .data(data)
+                .enter()
+                .append("circle")
+                .attr("class", function (d) {
+                    return "nodes " + d.eventid;
+                })
+                .attr("r", function (d) {
+                    return violenceScale(d.violence_policeorstate);
+                })
+                .attr("cx", width / 2)
+                .attr("cy", height / 2)
+                .style("fill", function (d) {
+                    return actionScale(d.action);
+                })
+
+            var simulation = d3.forceSimulation()
+                .force("x",
+                    d3.forceX()
+                    .strength(0.8)
+                    .x(function (d) {
+                        return timeScale(parseTime(+d.date));
+                    }))
+                .force("y",
+                    d3.forceY()
+                    .strength(0.1)
+                    .y(height / 2))
+                .force("charge", d3.forceManyBody().strength(0.8))
+                .force("collide", d3.forceCollide().strength(1)
+                    .radius(function (d) {
+                        return violenceScale(d.violence_policeorstate) + 1;
+                    })
+                    .iterations(2))
+
+            simulation
+                .nodes(data)
+                .on("tick", function (d) {
+                    node
+                        .attr("cx", function (d) {
+                            return d.x;
+                        })
+                        .attr("cy", function (d) {
+                            return d.y;
+                        })
+                });
+
+            node
+                .on("mouseover", function (event, d) {
+
+                    console.log(d);
+
+                    let tip = "<svg height='10' width='20'><polygon class='tip' points='10,0,20,10,0,10'/></svg>";
+
+                    tooltip.html("<div class='tipp'><div class='tip'>" + tip + "<p class='tooltiptext'>" + d.date_text + " (" + d.day_span + " days) <br><b>" + d.action + "</b> by " + d.initiator + " (" + d.initiator_type + ")<br>" + d.location + "</div></div>");
+
+                    tooltip
+                        .style("top", (+this.attributes.cy.value + 30) + "px")
+                        .style("left", (+this.attributes.cx.value - 100) + "px")
+                        .transition()
+                        .style("opacity", 1);
+
+                })
+                .on("mouseout", function (event, d) {
+                    tooltip.transition().style("opacity", 0).transition().style("top", 0 + "px")
+                })
+                .on("click", function (d) {
+                    let button = document.createElement("button");
+                    button.className = "policy bx--btn--primary btnn";
+                    button.innerHTML = d.intervention;
+
+                    document.getElementById("selection").appendChild(button);
+                    document.getElementById("newPolicy").style.display = "block";
+                    document.getElementById("pBuild").innerHTML = "Build";
+
+                    d3.select(this).transition().style("fill", "white");
+                })
+
+        });
     });
 
 };

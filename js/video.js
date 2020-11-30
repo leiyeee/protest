a7fadbc9315b3683311b6a78c9e8520065ceec3b
Lucy Yip <Lucy.Yip@ibm.com>
2020-11-29 20:51:39 -0500

    zxcvbnm

--- js/video.js
+++ js/video.js
@@ -1,11 +1,1 @@
-//$(window).scroll(function() {
-//    $('video').each(function() {
-//        if ($(this).visible(true)) {
-//            $(this)[0].play();
-//        } else {
-//            $(this)[0].pause();
-//        }
-//    })
-//});
-
 document.getElementById('myVideo').play();
